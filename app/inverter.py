2ee8eb5109f17432efd241579d4f865f508228c1
Asbjørn Kofoed-Nielsen <askn@jobindex.dk>
2023-02-26 11:52:56 +0100

    Add grid charge control

--- app/inverter.py
+++ app/inverter.py
@@ -2,7 +2,8 @@ import paho.mqtt.client as mqtt
 from config import config
 
 
-def set_time_of_use_voltage_point(point, voltage):
+
+def set_time_of_use(point, category, value):
     client = mqtt.Client("solar-controller")
     client.connect(config["solar_assistant"]["ip"])
-    client.publish(f"solar_assistant/inverter_1/voltage_point_{point}/set", voltage)
+    client.publish(f"solar_assistant/inverter_1/{category}_point_{point}/set", value)
